3e3eb64e34346cee5dd37dffc03b339d6f15f6dd
AndreyAA-code <otd53@yandex.ru>
2025-09-13 22:51:14 +0300

    ,all except popular

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
     private String error;
 
     public ErrorResponse(String error) {
         this.error = error;
     }
-    public String getError(){
-        return error;
-    }
+
 }
